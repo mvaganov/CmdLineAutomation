234c336f43ae6da751c6f599e515d7e16ba61081
Michael Vaganov <mvaganov@hotmail.com>
2024-06-02 01:35:29 -0700

    regex to read details from the command prompt

--- Packages/CmdLineAutomation/Runtime/CommandAutomationExecution.cs
+++ Packages/CmdLineAutomation/Runtime/CommandAutomationExecution.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace RunCmd {
@@ -54,9 +55,56 @@ namespace RunCmd {
 			/// </summary>
 			private StringBuilder outputToScan = new StringBuilder();
 
+			// TODO move this command line regex analysis to OperatingSystemCommandShell
+			// TODO make a data structure that can set variables by looking for specific regex patterns
+			private const string CommandPromptRegex =
+				"^[A-Z]:\\\\(?:[^\\\\/:*?\" <>|\\r\\n]+\\\\)*[^\\\\/:*? \"<>|\\r\\n]*>";
+
+			[System.Serializable] public class RegexSearch {
+				/// <summary>
+				/// Need help writing a regular expression? Ask ChatGPT! (I wonder how well this comment will age)
+				/// </summary>
+				public string Regex;
+				/// <summary>
+				/// leave empty to get the entire match
+				/// </summary>
+				public int[] GroupsToInclude;
+				public RegexSearch(string regex) : this(regex, null) { }
+				public RegexSearch(string regex, int[] groupsToInclude) {
+					Regex = regex;
+					GroupsToInclude = groupsToInclude;
+				}
+				public string Process(string input) {
+					Match m = System.Text.RegularExpressions.Regex.Match(input, Regex);
+					if (!m.Success) {
+						return null;
+					}
+					//Debug.LogWarning($"success {Regex}\n{input}\n{m.Value}");
+					if (GroupsToInclude == null || GroupsToInclude.Length == 0) {
+						return m.Value;
+					}
+					StringBuilder sb = new StringBuilder();
+					for(int i = 0; i < GroupsToInclude.Length; ++i) {
+						sb.Append(m.Groups[GroupsToInclude[i]]);
+					}
+					return sb.ToString();
+				}
+				public static implicit operator RegexSearch(string regex) => new RegexSearch(regex);
+			}
+
 			private void OutputAnalysis(string fromProcess) {
+				Dictionary<string, RegexSearch> regexSearches = new Dictionary<string, RegexSearch>() {
+					["WindowsTerminalVersion"] = new RegexSearch(@"Microsoft Windows \[Version ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\]", new int[]{ 1 }),
+					["dir"] = CommandPromptRegex,
+				};
 				outputToScan.AppendLine(fromProcess);
 				stdOutput?.Invoke(fromProcess);
+				foreach (var kvp in regexSearches) {
+					string value = kvp.Value.Process(fromProcess);
+					if (value != null) {
+						Debug.LogWarning($"success {kvp.Key}\n{fromProcess}\n{value}");
+					}
+				}
 			}
 
 			public IList<ParsedTextCommand> CommandsToDo {
