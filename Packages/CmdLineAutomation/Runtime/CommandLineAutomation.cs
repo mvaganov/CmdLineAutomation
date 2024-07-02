cbdcc19a5612396e716462c0dc436072f869dccf
Michael Vaganov <mvaganov@hotmail.com>
2024-07-01 23:42:24 -0700

    CmdLineAutomation replacement refactoring

---
+++ Packages/CmdLineAutomation/Runtime/CommandLineAutomation.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandLineAutomation
+{
+	// TODO replicate CmdLineAutomation, using CommandExecutor and CommandLinesettings
+}
