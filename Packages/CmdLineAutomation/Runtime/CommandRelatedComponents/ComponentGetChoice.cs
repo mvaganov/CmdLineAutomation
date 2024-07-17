dfa0dac893cc799ba571b7f5f600c4b5ebbf1db2
Michael Vaganov <mvaganov@hotmail.com>
2024-07-17 00:03:36 -0700

    Update ComponentGetChoice.cs

--- Packages/CmdLineAutomation/Runtime/CommandRelatedComponents/ComponentGetChoice.cs
+++ Packages/CmdLineAutomation/Runtime/CommandRelatedComponents/ComponentGetChoice.cs
@@ -2,9 +2,29 @@ using TMPro;
 using UnityEngine;
 
 public class ComponentGetChoice : MonoBehaviour {
-	public GameObject choicePrefab;
-	public TMP_Text message;
-	public RectTransform choiceContent;
+	[SerializeField] protected GameObject choicePrefab;
+	[SerializeField] protected TMP_Text message;
+	[SerializeField] protected RectTransform choiceContent;
+
+	public string Message {
+		get => message.text;
+		set => message.text = value;
+	}
+
+
+
+	public void SetChoiceText(int index, string text) {
+
+	}
+
+	public void SetChoiceCommand(int index, string command) {
+
+	}
+
+	public void SetChoice(int index, string text, string command) {
+		SetChoiceText(index, text);
+		SetChoiceCommand(index, command);
+	}
 
 	void Start() {
 
