b8c5f8cd4fe966b575db2f7e7f985a625fa44d9b
Michael Vaganov <mvaganov@hotmail.com>
2024-07-16 22:20:43 -0700

    GetChoice UI for runtime

---
+++ Packages/CmdLineAutomation/Runtime/CommandRelatedComponents/ComponentGetChoice.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+
+public class ComponentGetChoice : MonoBehaviour {
+	public GameObject choicePrefab;
+	public TMP_Text message;
+	public RectTransform choiceContent;
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+}
