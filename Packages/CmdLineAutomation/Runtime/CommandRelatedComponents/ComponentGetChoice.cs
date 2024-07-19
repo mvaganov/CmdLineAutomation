e58c431d991375c9be2130ec22d68472f280731c
Michael Vaganov <mvaganov@hotmail.com>
2024-07-18 23:31:04 -0700

    Update ComponentGetChoice.cs

--- Packages/CmdLineAutomation/Runtime/CommandRelatedComponents/ComponentGetChoice.cs
+++ Packages/CmdLineAutomation/Runtime/CommandRelatedComponents/ComponentGetChoice.cs
@@ -1,6 +1,7 @@
-using RunCmd;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RunCmd {
 	public class ComponentGetChoice : MonoBehaviour {
@@ -13,7 +14,8 @@ namespace RunCmd {
 			set => message.text = value;
 		}
 
-		public KeyValuePairStrings[] options;
+		public List<KeyValuePairStrings> options = new List<KeyValuePairStrings>();
+		public List<GameObject> optionUi;
 
 		public void SetChoiceText(int index, string text) {
 			options[index].Key = text;
@@ -23,9 +25,40 @@ namespace RunCmd {
 			options[index].Value = command;
 		}
 
-		public void SetChoice(int index, string text, string command) {
+		public void SetChoice(int index, string text, string command, object context, CommandGetChoice commandGetChoice) {
 			SetChoiceText(index, text);
 			SetChoiceCommand(index, command);
+			GameObject uiElement = optionUi[index];
+			TMP_Text textComponent = uiElement.GetComponentInChildren<TMP_Text>();
+			textComponent.text = text;
+			Button button = uiElement.GetComponentInChildren<Button>();
+			button.onClick.RemoveAllListeners();
+			button.onClick.AddListener(() => commandGetChoice.ChoiceMade(context, index));
+		}
+
+		public void SetChoicesUi(IEnumerable<KeyValuePairStrings> choices) {
+			options.Clear();
+			foreach(KeyValuePairStrings option in choices) {
+				options.Add(option);
+			}
+			RefreshOptions();
+		}
+
+		public void ClearOptionsUi() {
+			for(int i = 0; i < optionUi.Count; ++i) {
+				GameObject go = optionUi[i];
+				Destroy(go);
+			}
+		}
+		
+		public void RefreshOptions() {
+			ClearOptionsUi();
+			for (int i = 0; i < options.Count; ++i) {
+				GameObject optionUiElement = Instantiate(choicePrefab);
+				optionUiElement.transform.SetParent(choiceContent, false);
+				optionUi.Add(optionUiElement);
+				SetChoice(i, options[i].Key, options[i].Value);
+			}
 		}
 
 		void Start() {
