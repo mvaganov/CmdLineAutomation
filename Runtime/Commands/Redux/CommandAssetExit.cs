cf27267b9cf38adc4d4e1489b3b941edddfe6d57
Michael Vaganov <mvaganov@hotmail.com>
2024-09-06 21:21:22 -0700

    CommandAssetExit

---
+++ Runtime/Commands/Redux/CommandAssetExit.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace RunCmd {
+	[CreateAssetMenu(fileName = "exit", menuName = "ScriptableObjects/CommandAsset/exit")]
+	public class CommandAssetExit : ScriptableObject, ICommandAsset {
+		public ICommandProcess CreateCommand(object context) {
+			Proc proc = new Proc(context, this);
+			CommandManager.Instance.Add(context, this, proc);
+			return proc;
+		}
+
+		public class Proc : INamedProcess {
+			public CommandAssetExit source;
+			public object context;
+			public Proc(object context, CommandAssetExit source) { this.source = source; this.context = context; }
+
+			public string name => source.name;
+
+			public bool IsExecutionFinished => true;
+
+			public float GetProgress() => 1;
+
+			public void StartCooperativeFunction(string command, PrintCallback print) {
+				if (context is ICommandExecutor automation) {
+					automation.CancelProcess(context);
+				}
+				if (!OperatingSystemCommandShell.RunningShells.TryGetValue(context, out OperatingSystemCommandShell shell)) {
+					Debug.LogError($"no shell for '{context}'");
+					return;
+				}
+				shell.Exit();
+			}
+		}
+	}
+}
