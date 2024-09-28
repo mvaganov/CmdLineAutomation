056b00a30ebecfcd7b574d243dc92eb83c468d63
Michael Vaganov <mvaganov@hotmail.com>
2024-09-27 23:54:42 -0700

    Update ParseObject.cs

--- Runtime/DataStructures/ParseObject.cs
+++ Runtime/DataStructures/ParseObject.cs
@@ -1,6 +1,7 @@
 using RunCmd;
 using System;
 using System.Collections;
+using System.Reflection;
 using UnityEngine;
 using static RunCmd.Parse;
 
@@ -29,19 +30,30 @@ public class ParseObject : MonoBehaviour {
 		Debug.Log(resultData.ToString());
 	}
 
-	public static void Parse<TYPE>(string text, out Parse.ParseResult resultData, ref TYPE parsedObject) {
+	public static void Parse(string text, out Parse.ParseResult resultData, ref object parsedObject) {
 		object result = ParseText(text, out resultData);
 		Debug.Log(resultData.ToString());
-		Assign<TYPE>(ref parsedObject, result);
+		Assign(ref parsedObject, result);
 
 	}
-	public static void Assign<TYPE>(ref TYPE parsedObject, object data) {
-		Type type = typeof(TYPE);
+	public static void Assign(ref object parsedObject, object data) {
+		Type type = parsedObject.GetType();
 		RunCmd.Parse.ToString(data);
 
-
 		if (type.IsClass) {
-			Debug.Log($"class {type}");
+			if (data is IDictionary dict) {
+				foreach(DictionaryEntry kvp in dict) {
+					string name = kvp.Key.ToString();
+					Debug.Log(kvp.Key);
+					Debug.Log($"getting '{name}' from {parsedObject}");
+					object value = GetValue(parsedObject, name);
+					Assign(ref value, kvp.Value);
+					Debug.Log($"setting {name} = {value}");
+					SetValue(parsedObject, name, value);
+				}
+			} else {
+				Debug.Log($"can't create class {type} with {data}");
+			}
 		}
 		else if (type.IsArray) {
 			Debug.Log($"array {type}");
@@ -61,6 +73,33 @@ public class ParseObject : MonoBehaviour {
 			case IDictionary dict:
 				Debug.Log($"set {parsedObject} to {dict.Count} values");
 				break;
+		}
+	}
+
+	private static object GetValue(object source, string name) {
+		if (source == null) { return null; }
+		System.Type type = source.GetType();
+		BindingFlags bindFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
+		while (type != null) {
+			FieldInfo finfo = type.GetField(name, bindFlags);
+			if (finfo != null) { return finfo.GetValue(source); }
+			PropertyInfo pinfo = type.GetProperty(name, bindFlags | BindingFlags.IgnoreCase);
+			if (pinfo != null) { return pinfo.GetValue(source, null); }
+			type = type.BaseType;
+		}
+		return null;
+	}
+
+	private static void SetValue(object source, string name, object value) {
+		if (source == null) { return; }
+		System.Type type = source.GetType();
+		BindingFlags bindFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
+		while (type != null) {
+			FieldInfo field = type.GetField(name, bindFlags);
+			if (field != null) { field.SetValue(source, value); }
+			PropertyInfo prop = type.GetProperty(name, bindFlags | BindingFlags.IgnoreCase);
+			if (prop != null) { prop.SetValue(source, null); }
+			type = type.BaseType;
 		}
 	}
 
