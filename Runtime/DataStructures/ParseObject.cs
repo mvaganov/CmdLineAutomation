1a0d075a90bada7a29bfee3dc37a16459991fa61
Michael Vaganov <mvaganov@hotmail.com>
2024-09-28 23:07:51 -0700

    WIP parse object

--- Runtime/DataStructures/ParseObject.cs
+++ Runtime/DataStructures/ParseObject.cs
@@ -1,7 +1,9 @@
 using RunCmd;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 using static RunCmd.Parse;
 
@@ -33,38 +35,62 @@ public class ParseObject : MonoBehaviour {
 	public static void Parse(string text, out Parse.ParseResult resultData, ref object parsedObject) {
 		object result = ParseText(text, out resultData);
 		Debug.Log(resultData.ToString());
-		Assign(ref parsedObject, result);
+		Assign(ref parsedObject, parsedObject.GetType(), result);
 
 	}
-	public static void Assign(ref object parsedObject, object data) {
-		Type type = parsedObject.GetType();
+	public static void Assign(ref object parsedObject, Type objectType, object data) {
+		//Type type = parsedObject.GetType();
 		RunCmd.Parse.ToString(data);
 
-		if (type.IsClass) {
+		bool isString = objectType == typeof(string);
+
+		if (objectType.IsClass && !isString) {
 			if (data is IDictionary dict) {
 				foreach(DictionaryEntry kvp in dict) {
 					string name = kvp.Key.ToString();
 					Debug.Log(kvp.Key);
 					Debug.Log($"getting '{name}' from {parsedObject}");
-					object value = GetValue(parsedObject, name);
-					Assign(ref value, kvp.Value);
+					object value = GetValue(parsedObject, name, out Type valueType);
+					Assign(ref value, valueType, kvp.Value);
 					Debug.Log($"setting {name} = {value}");
 					SetValue(parsedObject, name, value);
 				}
 			} else {
-				Debug.Log($"can't create class {type} with {data}");
+					Debug.LogError($"can't create class {objectType} with {data}");
+				if (objectType.IsArray) {
+					IList iList = data as IList;
+					Debug.LogWarning("ARRAY! " + IsList(data));
+					if (iList != null) {
+						Debug.LogWarning(iList.Count);
+						Type arrayElementType = objectType.GetElementType();
+						Debug.LogWarning(arrayElementType);
+						Array arr = Array.CreateInstance(arrayElementType, iList.Count);
+						for(int i = 0; i < iList.Count; ++i) {
+							object elementData = iList[i];
+							object element = Activator.CreateInstance(arrayElementType);
+							arr.SetValue(element, i);
+							Assign(ref element, arrayElementType, elementData);
+							arr.SetValue(element, i);
+						}
+						parsedObject = arr;
+					}
+				}
 			}
 		}
-		else if (type.IsArray) {
-			Debug.Log($"array {type}");
-		} else if (type.IsValueType) {
-			Debug.Log($"value {type}");
+		else if (objectType.IsArray) {
+			Debug.Log($"array {objectType}");
+		} else if (objectType.IsValueType || isString) {
+			if (data is RunCmd.Parse.Token token) {
+				parsedObject = Convert.ChangeType(token.Text, objectType);
+			} else {
+				Debug.LogError($"unable to set {objectType} to {data}");
+			}
 		} else {
-			Debug.Log($"??? {type}");
+			Debug.Log($"??? {objectType}");
 		}
 
 		switch (data) {
-			case Token token:
+			case RunCmd.Parse.Token token:
 				Debug.Log($"set {parsedObject} to {token}");
 				break;
 			case IList list:
@@ -76,17 +102,18 @@ public class ParseObject : MonoBehaviour {
 		}
 	}
 
-	private static object GetValue(object source, string name) {
-		if (source == null) { return null; }
+	private static object GetValue(object source, string name, out Type resultType) {
+		if (source == null) { resultType = typeof(int); return null; }
 		System.Type type = source.GetType();
 		BindingFlags bindFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
 		while (type != null) {
-			FieldInfo finfo = type.GetField(name, bindFlags);
-			if (finfo != null) { return finfo.GetValue(source); }
-			PropertyInfo pinfo = type.GetProperty(name, bindFlags | BindingFlags.IgnoreCase);
-			if (pinfo != null) { return pinfo.GetValue(source, null); }
+			FieldInfo field = type.GetField(name, bindFlags);
+			if (field != null) { resultType = field.FieldType; return field.GetValue(source); }
+			PropertyInfo prop = type.GetProperty(name, bindFlags | BindingFlags.IgnoreCase);
+			if (prop != null) { resultType = prop.PropertyType; return prop.GetValue(source, null); }
 			type = type.BaseType;
 		}
+		resultType = typeof(byte);
 		return null;
 	}
 
@@ -103,6 +130,12 @@ public class ParseObject : MonoBehaviour {
 		}
 	}
 
+	public static bool IsList(object o) {
+		if (o == null) return false;
+		return o is IList &&
+					 o.GetType().IsGenericType &&
+					 o.GetType().GetGenericTypeDefinition().IsAssignableFrom(typeof(List<>));
+	}
 	void Start() {
 
 	}
