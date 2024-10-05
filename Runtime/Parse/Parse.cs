00c1b807b616e85996583cf4d76dd1ac6af74ef9
Michael Vaganov <mvaganov@hotmail.com>
2024-10-04 20:08:49 -0700

    Update Parse.cs

--- Runtime/Parse/Parse.cs
+++ Runtime/Parse/Parse.cs
@@ -196,11 +196,36 @@ namespace RunCmd {
 
 		private static void ToStringArray(StringBuilder sb, IList list, int indent, bool includeWhitespace) {
 			sb.Append("[");
+			int lines = 0;
 			for (int i = 0; i < list.Count; ++i) {
 				if (i > 0) { sb.Append(includeWhitespace ? ", " : ","); }
-				sb.Append(ToString(list[i], indent + 1, includeWhitespace));
+				string element = ToString(list[i], indent + 1, includeWhitespace);
+				if (includeWhitespace) { PossiblyIndent(sb, element, indent); }
+				sb.Append(element);
+				lines += Count(element, "\n");
+			}
+			if (includeWhitespace && lines > 0) {
+				sb.Append("\n");
+				IndentInternal(sb, indent);
 			}
 			sb.Append("]");
+		}
+
+		private static void PossiblyIndent(StringBuilder sb, string element, int indent) {
+			if ("[{".IndexOf(element[0]) >= 0 && Count(element, "\n") > 0) {
+				sb.Append("\n");
+				IndentInternal(sb, indent + 1);
+			}
+		}
+
+		private static int Count(string haystack, string needle) {
+			int i = -1, count = 0;
+			while (i < haystack.Length) {
+				i = haystack.IndexOf(needle, i+1);
+				if (i < 0) { break; }
+				++count;
+			}
+			return count;
 		}
 
 		private static void ToStringDictionary(StringBuilder sb, IDictionary dict, int indent, bool includeWhitespace) {
@@ -211,18 +236,25 @@ namespace RunCmd {
 				if (addedOne) { sb.Append(","); }
 				PossibleWhiteSpaceAfterKeyValuePair();
 				sb.Append(ToString(kvp.Key, indent + 1, includeWhitespace))
-					.Append(includeWhitespace ? " : " : ":").Append(ToString(kvp.Value, indent + 1));
+					.Append(includeWhitespace ? " : " : ":");
+				string element = ToString(kvp.Value, indent + 1, includeWhitespace);
+				if (includeWhitespace) { PossiblyIndent(sb, element, indent); }
+				sb.Append(element);
 				addedOne = true;
 			}
 			--indent;
 			PossibleWhiteSpaceAfterKeyValuePair();
-			sb.Append(includeWhitespace ? "}\n" : "}");
+			sb.Append("}");
 			
 			void PossibleWhiteSpaceAfterKeyValuePair() {
 				if (!includeWhitespace) { return; }
 				sb.Append("\n");
-				for (int i = 0; i < indent; ++i) { sb.Append("  "); }
+				IndentInternal(sb, indent);
 			}
+		}
+
+		private static void IndentInternal(StringBuilder sb, int indent) {
+			for (int i = 0; i < indent; ++i) { sb.Append("  "); }
 		}
 	}
 }
