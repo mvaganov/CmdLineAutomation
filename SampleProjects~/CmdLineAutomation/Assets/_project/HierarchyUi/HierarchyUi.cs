71a121a86ed9750523ae6cd574efe4928a6a74c1
Michael Vaganov <mvaganov@hotmail.com>
2025-01-10 00:51:50 -0800

    hierarchy calculation

--- SampleProjects~/CmdLineAutomation/Assets/_project/HierarchyUi/HierarchyUi.cs
+++ SampleProjects~/CmdLineAutomation/Assets/_project/HierarchyUi/HierarchyUi.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,14 +9,17 @@ public class HierarchyUi : MonoBehaviour {
 	public class ElementState {
 		[HideInInspector]
 		public string name;
-		public Transform target;
+		public int column, row, height;
 		public ElementState parent;
+		[HideInInspector]
+		public Transform target;
 		//public HierarchyElement ui;
+		[HideInInspector]
 		public Button label;
+		[HideInInspector]
 		public Button expand; 
 		public bool expanded;
 		public List<ElementState> children = new List<ElementState>();
-		public int column, row, height;
 
 		public ElementState(ElementState parent, Transform target, int column, int row, bool expanded) {
 			this.target = target;
@@ -39,26 +43,32 @@ public class HierarchyUi : MonoBehaviour {
 		private void RefreshHeight() {
 			int depth = 0;
 			if (parent != null) {
-				parent.CalculateHeight(ref depth);
+				parent.CalculateHeight(0, ref depth);
 			} else {
-				CalculateHeight(ref depth);
+				CalculateHeight(0, ref depth);
 			}
 		}
 
 		public int CalculateHeight() {
 			int depth = 0;
-			return CalculateHeight(ref depth);
+			return CalculateHeight(0, ref depth);
 		}
 
-		public int CalculateHeight(ref int maxDepth) {
-			height = 1;
+		public int CalculateHeight(int depth, ref int maxDepth) {
+			if (depth > maxDepth) {
+				maxDepth = depth;
+			}
+			height = parent != null ? 1 : 0;
 			if (children.Count == 0) {
 				return height;
 			}
 			if (expanded) {
-				++maxDepth;
+				int r = row + 1;
 				for (int i = 0; i < children.Count; i++) {
-					height += children[i].CalculateHeight(ref maxDepth);
+					children[i].row = r;
+					int elementHeight = children[i].CalculateHeight(depth+1, ref maxDepth);
+					height += elementHeight;
+					r += elementHeight;
 				}
 			}
 			return height;
@@ -81,7 +91,7 @@ public class HierarchyUi : MonoBehaviour {
 	public HierarchyElement prefab;
 	public ContentSizeFitter contentPanel;
 	public Vector2 contentSize;
-	public List<HierarchyElement> pool = new List<HierarchyElement>();
+	public List<GameObject> pool = new List<GameObject>();
 	public Button prefabElement;
 	public Button prefabExpand;
 
@@ -98,25 +108,56 @@ public class HierarchyUi : MonoBehaviour {
 		float elementWidth = prefabElement.GetComponent<RectTransform>().sizeDelta.x;
 		float indentWidth = prefabExpand.GetComponent<RectTransform>().sizeDelta.x;
 		int depth = 0;
-		root.CalculateHeight(ref depth);
+		root.CalculateHeight(0, ref depth);
 		contentSize.y = root.height * elementHeight;
 		contentSize.x = depth * indentWidth + elementWidth;
+		RectTransform rt = contentPanel.GetComponent<RectTransform>();
+		rt.sizeDelta = contentSize;
+		ClearAllUi();
+		CreateAllChildren(root);
+	}
 
+	private void ClearAllUi() {
+		pool.ForEach(e => DestroyImmediate(e));
+		pool.Clear();
+	}
+
+	private void CreateAllChildren(ElementState es) {
+		for (int i = 0; i < es.children.Count; i++) {
+			ElementState child = es.children[i];
+			Debug.Log($"{i} creating {child.name}");
+			CreateElement(child);
+			CreateAllChildren(child);
+		}
 	}
 
 	private void CreateElement(ElementState es) {
 		float elementHeight = prefabElement.GetComponent<RectTransform>().sizeDelta.y;
 		float indentWidth = prefabExpand.GetComponent<RectTransform>().sizeDelta.x;
-		Vector2 cursor = new Vector2(indentWidth * es.column, elementHeight * es.row);
+		Vector2 cursor = new Vector2(indentWidth * es.column, -elementHeight * es.row);
 		Vector2 elementPosition = cursor + Vector2.right * indentWidth;
-		GameObject element = Instantiate(prefabElement.gameObject);
-		element.transform.SetParent(contentPanel.transform, false);
-		element.transform.localPosition = elementPosition;
+		Transform content = contentPanel.transform;
+		RectTransform rt;
 		if (es.children.Count > 0) {
 			GameObject expand = Instantiate(prefabExpand.gameObject);
-			expand.transform.SetParent(expand.transform, false);
-			expand.transform.localPosition = cursor;
+			rt = expand.GetComponent<RectTransform>();
+			rt.SetParent(content, false);
+			rt.anchoredPosition = cursor;
+			rt.name = $"> {es.name}";
+			es.expand = expand.GetComponent<Button>();
+			expand.gameObject.SetActive(true);
+			pool.Add(expand);
 		}
+		GameObject element = Instantiate(prefabElement.gameObject);
+		rt = element.GetComponent<RectTransform>();
+		rt.SetParent(content, false);
+		rt.anchoredPosition = elementPosition;
+		rt.name = $"({es.name})";
+		es.label = element.GetComponent<Button>();
+		element.gameObject.SetActive(true);
+		TMP_Text text = element.GetComponentInChildren<TMP_Text>();
+		text.text = es.name;
+		pool.Add(element);
 	}
 
 	void Start() {
@@ -133,7 +174,7 @@ public class HierarchyUi : MonoBehaviour {
 		List<Transform> list = new List<Transform> ();
 		for (int i = 0; i < all.Length; ++i) {
 			Transform t = all[i];
-			if (t != null && t.parent == null) {
+			if (t != null && t.parent == null && t.GetComponent<HierarchyIgnore>() == null) {
 				list.Add(t);
 			}
 		}
@@ -144,12 +185,13 @@ public class HierarchyUi : MonoBehaviour {
 	public void RefreshRoot() {
 		List<Transform> list = GetAllElements();
 		bool expanded = true;
-		root = new ElementState(null, null, 0, 0, expanded);
+		root = new ElementState(null, null, 0, -1, expanded);
 		for(int i = 0; i < list.Count; ++i) {
 			ElementState es = new ElementState(root, list[i], 0, i, expanded);
 			root.children.Add(es);
 			es.AddChildren(expanded);
 		}
-		root.CalculateHeight();
+		//root.CalculateHeight();
+		CalculateHierarchy();
 	}
 }
